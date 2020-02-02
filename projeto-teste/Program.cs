1f8934c4463cf79b1e6559db8c45b9b85656da22
George De Luca <george@deluca.eti.br>
2020-02-02 12:45:21 +0000

    Acrescentou tracos

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -6,10 +6,12 @@ namespace projeto_teste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("------------------------------");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Como vcê está hoje?");
             Console.WriteLine("Tenha um bom dia");
             Console.WriteLine("Tchau");
+            Console.WriteLine("------------------------------");
             Console.ReadLine();
         }
     }
